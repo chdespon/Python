bd5e2faf2a2f54c2fc07742fa0379005fd0358bb
chdespon <chdespon@student.42.fr>
2023-12-07 18:18:46 +0100

    ex00, 01, 02, done

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,11 @@
+import time
+# Write a script that formats the dates this way "Seconds since January 1, 1970: 1,666,355,857.3622 or time in scientific notation$
+# Oct 21 2022$", of course your date will not be mine
+# as in the example but it must be formatted the same
+
+
+if __name__ == '__main__':
+	timeS = time.time()
+	timeF = f"{timeS: ,}"
+	print("Seconds since January 1, 1970: " + str(timeF) + f" or {timeS:.2e} " + " in scientific notation")
+	print(time.strftime("%b %d %Y", time.localtime()))
---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,6 @@
+# Write a function that prints the object type of all types of "Null".
+# Return 0 if it goes well and 1 in case of error.
+# Your function needs to print all types of NULL.
+
+def NULL_not_found(object: any) -> int:
+	
