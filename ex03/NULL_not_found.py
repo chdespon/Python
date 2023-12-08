5338e8e8275837ac559bd74a741a53be37a62031
chdespon <chdespon@student.42.fr>
2023-12-08 16:37:28 +0100

    ex03

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -3,4 +3,16 @@
 # Your function needs to print all types of NULL.
 
 def NULL_not_found(object: any) -> int:
-	
+	dictionary = { float : "Cheese: nan",
+					type(None) : "Nothing: None",
+					int : "Zero: 0",
+					str : "Empty: ",
+					bool : "Fake: False"}
+
+	# if is in dictionary and is null, print
+	if dictionary.get(type(object)) and (not object or object != object):
+		print(dictionary.get(type(object)), type(object))
+	else:
+		print("Type not found")
+		return 1
+	return 0
