72c0aed483ca65a193a71187ba3c125b8767d0fe
chdespon <chdespon@student.42.fr>
2024-11-27 18:49:17 +0100

    modifie function is_int try to convert number into int instead of cheaking number type

---
+++ 0-Starting/ex04/whatis.py
@@ -0,0 +1,36 @@
+# Create a script that takes a number as argument,
+# checks whether it is odd or even,
+# and prints the result.
+# If more than one argument is provided or if the argument is not an integer,
+# print an AssertionError.
+
+import sys
+
+
+def is_int(number: any, type: type) -> bool:
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+
+def whatis(number: int) -> None:
+    if number % 2 == 0:
+        print("I'm Even")
+    else:
+        print("I'm Odd")
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        if len(sys.argv) == 1:
+            sys.exit(0)
+        assert is_int((sys.argv[1]), int), "argument is not an integer"
+        whatis(int(sys.argv[1]))
+    except AssertionError as e:
+        print("AssertionError:", e)
+        sys.exit(1)
+    sys.exit(0)
